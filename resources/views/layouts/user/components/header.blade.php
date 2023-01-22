8d5592366744f4a563f454571d534e9827220b9c
candrabudi <candrasachio@gmail.com>
2023-01-22 08:23:00 +0700

    logout

--- resources/views/layouts/user/components/header.blade.php
+++ resources/views/layouts/user/components/header.blade.php
@@ -58,10 +58,13 @@
                                 <div class="dropdown-divider"></div>
                             </li>
                             <li>
-                                <a class="dropdown-item" href="auth-login-cover.html" target="_blank">
-                                    <i class="ti ti-logout me-2 ti-sm"></i>
-                                    <span class="align-middle">Log Out</span>
-                                </a>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button class="dropdown-item" href="auth-login-cover.html" target="_blank">
+                                        <i class="ti ti-logout me-2 ti-sm"></i>
+                                        <span class="align-middle">Log Out</span>
+                                    </button>
+                                </form>
                             </li>
                         </ul>
                     </li>
